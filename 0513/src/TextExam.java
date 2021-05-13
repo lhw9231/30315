a7c3ca4d22cf8ac821dd0117844f1706dd5cac74
30315 <30315>
2021-05-13 15:01:03 +0900

    문제 못 풀음 ㅠㅠ

---
+++ 0513/src/TextExam.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TextExam {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		int b;
+		
+		System.out.println("500이하의 자연수를 입력하시오" );
+		a = sc.nextInt(); 
+		
+		for(int i=0;i<a;i++){
+			if(a/7==0){
+				
+			}
+		}
+		
+		
+	}
+
+}
